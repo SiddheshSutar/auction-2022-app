0f437a6e0a3046d200c92a1cc1745d07224d754e
Siddhesh Sutar <siddheshsutar96@gmail.com>
2024-01-29 21:33:21 +0530

    BE list

--- src/externalLists/ListOfTeams.js
+++ src/externalLists/ListOfTeams.js
@@ -2,78 +2,155 @@ import { MAX_AMOUNT } from "../helpers"
 
 const teams = [
     {
-        Name : 'Shashi Amin',
-        Owner : 'OwnerName1',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#272e9b",
-        id: 0
+        "Name" : 'Shashi Amin',
+        "Owner" : 'OwnerName1',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#272e9b",
+        "id": 0
     },
     {
-        Name : 'Manohar Pawar',
-        Owner : 'OwnerName2',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#21bd21",
-        id: 1
+        "Name" : 'Manohar Pawar',
+        "Owner" : 'OwnerName2',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#21bd21",
+        "id": 1
     },
     {
-        Name : 'Ravindra Mane',
-        Owner : 'OwnerName3',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#a3a304",
-        id: 2
+        "Name" : 'Ravindra Mane',
+        "Owner" : 'OwnerName3',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#a3a304",
+        "id": 2
     },
     {
-        Name : 'Pravin Sawant',
-        Owner : 'OwnerName4',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#a63def",
-        id: 3
+        "Name" : 'Pravin Sawant',
+        "Owner" : 'OwnerName4',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#a63def",
+        "id": 3
     },
     {
-        Name : 'Manisha Mhapralkar',
-        Owner : 'OwnerName5',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#14a5f9",
-        id: 4
+        "Name" : 'Manisha Mhapralkar',
+        "Owner" : 'OwnerName5',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#14a5f9",
+        "id": 4
     },
     {
-        Name : 'Pradeep Gode',
-        Owner : 'OwnerName6',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#f95d12",
-        id: 5
+        "Name" : 'Pradeep Gode',
+        "Owner" : 'OwnerName6',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#f95d12",
+        "id": 5
     },
     {
-        Name : 'Sudhakar Gosavi',
-        Owner : 'OwnerName7',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#004c5a",
-        id: 6
+        "Name" : 'Sudhakar Gosavi',
+        "Owner" : 'OwnerName7',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#004c5a",
+        "id": 6
     },
     {
-        Name : 'Sunita Sawant',
-        Owner : 'OwnerName8',
-        Players : [],
-        Amount_Used : 0,
-        Amount_Assigned: MAX_AMOUNT,
-        Color: "#686868",
-        id: 7
+        "Name" : 'Sunita Sawant',
+        "Owner" : 'OwnerName8',
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": MAX_AMOUNT,
+        "Color": "#686868",
+        "id": 7
     },
 
 ]
+
+const payload = [
+    {
+        "Name" : "Shashi Amin",
+        "Owner" : "OwnerName1",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#272e9b",
+        "idEr": 0
+    },
+    {
+        "Name" : "Manohar Pawar",
+        "Owner" : "OwnerName2",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#21bd21",
+        "idEr": 1
+    },
+    {
+        "Name" : "Ravindra Mane",
+        "Owner" : "OwnerName3",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#a3a304",
+        "idEr": 2
+    },
+    {
+        "Name" : "Pravin Sawant",
+        "Owner" : "OwnerName4",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#a63def",
+        "idEr": 3
+    },
+    {
+        "Name" : "Manisha Mhapralkar",
+        "Owner" : "OwnerName5",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#14a5f9",
+        "idEr": 4
+    },
+    {
+        "Name" : "Pradeep Gode",
+        "Owner" : "OwnerName6",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#f95d12",
+        "idEr": 5
+    },
+    {
+        "Name" : "Sudhakar Gosavi",
+        "Owner" : "OwnerName7",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#004c5a",
+        "idEr": 6
+    },
+    {
+        "Name" : "Sunita Sawant",
+        "Owner" : "OwnerName8",
+        "Players" : [],
+        "Amount_Used" : 0,
+        "Amount_Assigned": 100,
+        "Color": "#686868",
+        "idEr": 7
+    }
+
+]
+
 
 export default teams
